e67e7db855f0b47b3bf2c8c4aeea42d27d81cccf
Omar Arias <omarariasrocha@hotmail.com>
2025-02-27 15:08:33 -0600

    labs

---
+++ python essentials 1/lab3.1.10.py
@@ -0,0 +1,7 @@
+inputt=input()
+if inputt=="Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif inputt=="spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not",inputt,"!")
